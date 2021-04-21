f1f601ed4fc4136386a412ac278b4773473b2e06
Davide <69810644+ItzDavi@users.noreply.github.com>
2021-04-21 23:19:06 +0200

    Update index.js
    Added nextSong and prevSong functions, classes removes and adds still bugged

--- Music Player v1.1/index.js
+++ Music Player v1.1/index.js
@@ -1,19 +1,18 @@
-const musicContainer = document.querySelector('.music-container');
+const musicContainer = document.getElementById('music-container');
 
-const playBtn = document.querySelector('#play');
-const prevBtn = document.querySelector('#prev');
-const nextBtn = document.querySelector('#next');
+const playBtn = document.getElementById('play');
+const prevBtn = document.getElementById('prev');
+const nextBtn = document.getElementById('next');
 
-const audio = document.querySelector('#audio');
+const audio = document.getElementById('audio');
 
-const progressContainer = document.querySelector('.progress-container');
-const progress = document.querySelector('.progress');
+const progressContainer = document.getElementById('progress-container');
+const progress = document.getElementById('progress');
 
-const title = document.querySelector('#title');
-const thumbnail = document.querySelector('#thumbnail');
+const title = document.getElementById('title');
+const thumbnail = document.getElementById('thumbnail');
 
 const songs = ['track', 'track-2', 'track-3'];
-const thumbnails = ['thumbnail', 'thumbnail-2', 'thumbnail-3'];
 
 let songIndex = 0;
 
@@ -26,20 +25,54 @@ function loadSong(song) {
 }
 
 function playSong() {
-  musicContainer.classList.add("play");
+  playBtn.classList.add("play");
+  playBtn.querySelector('i.fas').classList.remove('fa-play');
+  playBtn.querySelector('i.fas').classList.add('fa-pause');
+
+  audio.play();
 }
 
 function pauseSong() {
-  musicContainer.classList.remove("play");
+  playBtn.classList.remove("play");
+  playBtn.querySelector('i.fas').classList.add('fa-play');
+  playBtn.querySelector('i.fas').classList.remove('fa-pause');
+
+  audio.pause();
 }
 
-playBtn.addEventListener("click", event => {
-  console.log("gsdhd");
-  const isPlaying = musicContainer.classList.contains("play");
+function prevSong() {
+  songIndex--;
 
-  if (isPlaying) {
+  if (songIndex < 0) {
+    songIndex = songs.length - 1;
+  }
+
+  loadSong(songs[songIndex]);
+  playSong();
+}
+
+function nextSong() {
+  songIndex++;
+
+  if (songIndex > songs.length - 1) {
+    songIndex = 0;
+  }
+
+  loadSong(songs[songIndex]);
+  playSong();
+}
+
+playBtn.addEventListener("click", () => {
+  console.log("click");
+  const isPlaying = playBtn.classList.contains("play");
+
+  if (playBtn.classList.contains("play")) {
     pauseSong();
   } else {
     playSong();
   }
 });
+
+
+prevBtn.addEventListener("click", prevSong);
+nextBtn.addEventListener("click", nextSong);
